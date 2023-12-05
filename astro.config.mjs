356edee1ddbb46814173d977b06cd3800380cf29
Vincenzo Mancinelli <v.mancinelli@soes.it>
2023-12-05 17:03:10 +0100

    new url

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,8 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mancio8.github.io',
-  base: '/cv',
+  site: 'https://mancio8.github.io/cv',
+  base: '/',
     integrations: [mdx(), sitemap(), tailwind(), image(
     {
       serviceEntryPoint: '@astrojs/image/sharp',
