be4f91c94e1c0496f88b8b49b63c768d0ad8792a
mancio8 <mancinelliasr8@gmail.com>
2024-10-29 14:50:39 +0100

    new f1 stats

--- src/services/f1Service.ts
+++ src/services/f1Service.ts
@@ -1,7 +1,7 @@
 // src/services/f1Service.ts
 
 export async function getNextRace() {
-    const response = await fetch('https://ergast.com/api/f1/current/next.json', { cache: "no-store" });
+    const response = await fetch('https://api.jolpi.ca/ergast/f1/current/next.json', { cache: "no-store" });
     const data = await response.json();
     const race = data.MRData.RaceTable.Races[0];
 
@@ -19,7 +19,7 @@ export async function getNextRace() {
     // Controllo per i risultati delle qualifiche
     let qualifyingResults = [];
     try {
-        const qualResultsResponse = await fetch(`https://ergast.com/api/f1/current/${race.round}/qualifying.json`, { cache: "no-store" });
+        const qualResultsResponse = await fetch(`https://api.jolpi.ca/ergast/f1/current/${race.round}/qualifying.json`, { cache: "no-store" });
         const qualResultsData = await qualResultsResponse.json();
         qualifyingResults = qualResultsData.MRData.RaceTable.Races[0].QualifyingResults;
     } catch (error) {
@@ -35,13 +35,13 @@ export async function getNextRace() {
 }
 
 export async function getLastRaceResults() {
-    const response = await fetch('https://ergast.com/api/f1/current/last/results.json', { cache: "no-store" });
+    const response = await fetch('https://api.jolpi.ca/ergast/f1/current/last/results.json', { cache: "no-store" });
     const data = await response.json();
     return data.MRData.RaceTable.Races[0].Results;
 }
 
 export async function getDriverStandings() {
-    const response = await fetch('https://ergast.com/api/f1/current/driverStandings.json', { cache: "no-store" });
+    const response = await fetch('https://api.jolpi.ca/ergast/f1/current/driverStandings.json', { cache: "no-store" });
     const data = await response.json();
     
     // Estrai le informazioni sui piloti e i team
@@ -62,37 +62,37 @@ export async function getDriverStandings() {
 
 
 export async function getConstructorStandings() {
-    const response = await fetch('https://ergast.com/api/f1/current/constructorStandings.json', { cache: "no-store" });
+    const response = await fetch('https://api.jolpi.ca/ergast/f1/current/constructorStandings.json', { cache: "no-store" });
     const data = await response.json();
     return data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
 }
 
 export async function getAllRacesInSeason(season) {
-    const response = await fetch(`https://ergast.com/api/f1/${season}.json`, { cache: "no-store" });
+    const response = await fetch(`https://api.jolpi.ca/ergast/f1/${season}.json`, { cache: "no-store" });
     const data = await response.json();
     return data.MRData.RaceTable.Races;
 }
 
 export async function getConstructorInfo(constructorId) {
-    const response = await fetch(`https://ergast.com/api/f1/constructors/${constructorId}.json`, { cache: "no-store" });
+    const response = await fetch(`https://api.jolpi.ca/ergast/f1/constructors/${constructorId}.json`, { cache: "no-store" });
     const data = await response.json();
     return data.MRData.ConstructorTable.Constructors[0];
 }
 
 export async function getDriverInfo(driverId) {
-    const response = await fetch(`https://ergast.com/api/f1/drivers/${driverId}.json`, { cache: "no-store" });
+    const response = await fetch(`https://api.jolpi.ca/ergast/f1/drivers/${driverId}.json`, { cache: "no-store" });
     const data = await response.json();
     return data.MRData.DriverTable.Drivers[0];
 }
 
 export async function getCircuitInfo(circuitId) {
-    const response = await fetch(`https://ergast.com/api/f1/circuits/${circuitId}.json`, { cache: "no-store" });
+    const response = await fetch(`https://api.jolpi.ca/ergast/f1/circuits/${circuitId}.json`, { cache: "no-store" });
     const data = await response.json();
     return data.MRData.CircuitTable.Circuits[0];
 }
 
 export async function getRaceDetails(season, round) {
-    const response = await fetch(`https://ergast.com/api/f1/${season}/${round}.json`, { cache: "no-store" });
+    const response = await fetch(`https://api.jolpi.ca/ergast/f1/${season}/${round}.json`, { cache: "no-store" });
     const data = await response.json();
     return data.MRData.RaceTable.Races[0];
 }
@@ -104,13 +104,13 @@ export async function getDriverStatsAll(driverId: string) {
     let championships = 0;
 
     // 1. Ottieni le stagioni in cui il pilota ha gareggiato
-    const seasonsResponse = await fetch(`https://ergast.com/api/f1/drivers/${driverId}/seasons.json?limit=100`);
+    const seasonsResponse = await fetch(`https://api.jolpi.ca/ergast/f1/drivers/${driverId}/seasons.json?limit=100`);
     const seasonsData = await seasonsResponse.json();
     const seasons = seasonsData.MRData.SeasonTable.Seasons.map((season) => season.season);
 
     for (const season of seasons) {
         // 2. Ottieni i risultati per la stagione
-        const seasonResponse = await fetch(`https://ergast.com/api/f1/${season}/drivers/${driverId}/results.json`);
+        const seasonResponse = await fetch(`https://api.jolpi.ca/ergast/f1/${season}/drivers/${driverId}/results.json`);
         const seasonData = await seasonResponse.json();
         const races = seasonData.MRData.RaceTable.Races;
 
@@ -128,7 +128,7 @@ export async function getDriverStatsAll(driverId: string) {
         }
 
         // 3. Verifica se il pilota ha vinto il campionato in quella stagione
-        const standingsResponse = await fetch(`https://ergast.com/api/f1/${season}/drivers/${driverId}/driverStandings.json`);
+        const standingsResponse = await fetch(`https://api.jolpi.ca/ergast/f1/${season}/drivers/${driverId}/driverStandings.json`);
         const standingsData = await standingsResponse.json();
         const standingsList = standingsData.MRData.StandingsTable.StandingsLists;
 
@@ -152,7 +152,7 @@ export async function getDriverStats(driverId: string) {
     let championships = 0;
 
     // Ottieni i risultati per la stagione 2024
-    const seasonResponse = await fetch(`https://ergast.com/api/f1/2024/drivers/${driverId}/results.json`);
+    const seasonResponse = await fetch(`https://api.jolpi.ca/ergast/f1/2024/drivers/${driverId}/results.json`);
     const seasonData = await seasonResponse.json();
     const races = seasonData.MRData.RaceTable.Races;
 
@@ -170,7 +170,7 @@ export async function getDriverStats(driverId: string) {
     }
 
     // Verifica se il pilota ha vinto il campionato nella stagione 2024
-    const standingsResponse = await fetch(`https://ergast.com/api/f1/2024/drivers/${driverId}/driverStandings.json`);
+    const standingsResponse = await fetch(`https://api.jolpi.ca/ergast/f1/2024/drivers/${driverId}/driverStandings.json`);
     const standingsData = await standingsResponse.json();
     const standingsList = standingsData.MRData.StandingsTable.StandingsLists;
 
@@ -186,3 +186,66 @@ export async function getDriverStats(driverId: string) {
     };
 }
 
+export async function getConstructorStats(constructorId: string) {
+    let wins = 0;
+    let podiums = 0;
+
+    // Prima chiamata per ottenere i primi 30 risultati
+    const initialResponse = await fetch(`https://api.jolpi.ca/ergast/f1/2024/constructors/${constructorId}/results.json`);
+    const initialData = await initialResponse.json();
+    const races = initialData.MRData.RaceTable.Races;
+
+    // Conta le vittorie e i podi dai risultati iniziali
+    for (const race of races) {
+        // Ignora le gare sprint
+        if (race.raceName.toLowerCase().includes('sprint')) continue;
+
+        for (const result of race.Results) {
+            if (result.Constructor.constructorId === constructorId) {
+                const position = parseInt(result.position, 10);
+                if (position === 1) wins++;
+                if (position >= 1 && position <= 3) podiums++;
+            }
+        }
+    }
+
+    // Seconda chiamata per ottenere ulteriori risultati, se esistono
+    let offset = 30;
+    let hasMoreResults = true;
+
+    while (hasMoreResults) {
+        const response = await fetch(`https://api.jolpi.ca/ergast/f1/2024/constructors/${constructorId}/results.json?offset=${offset}`);
+        const data = await response.json();
+        const additionalRaces = data.MRData.RaceTable.Races;
+
+        // Controlla se ci sono risultati
+        if (!additionalRaces || additionalRaces.length === 0) {
+            hasMoreResults = false;
+            continue;
+        }
+
+        // Conta le vittorie e i podi dai risultati aggiuntivi
+        for (const race of additionalRaces) {
+            if (race.raceName.toLowerCase().includes('sprint')) continue;
+
+            for (const result of race.Results) {
+                if (result.Constructor.constructorId === constructorId) {
+                    const position = parseInt(result.position, 10);
+                    if (position === 1) wins++;
+                    if (position >= 1 && position <= 3) podiums++;
+                }
+            }
+        }
+
+        // Incrementa l'offset per la prossima chiamata
+        offset += 30;
+
+        // Verifica se ci sono più risultati
+        hasMoreResults = additionalRaces.length === 30;
+    }
+
+    return {
+        wins,
+        podiums
+    };
+}
